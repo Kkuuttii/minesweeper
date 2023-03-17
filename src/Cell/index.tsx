3dc3f961fe49ac4d172f063e85362c6e3d72c6c9
Kkuuttii <katerinaugolek@gmail.com>
2023-03-17 19:02:29 +0300

    Made cells clickable

--- src/Cell/index.tsx
+++ src/Cell/index.tsx
@@ -1,7 +1,22 @@
 import styles from "./index.module.scss";
+import { useState } from "react";
 
-function Cell() {
-  return <div className={styles.cell}></div>;
+interface ICell {
+  onClick: () => void;
+}
+
+function Cell({ onClick }: ICell) {
+  const [clicked, setClicked] = useState<boolean>(false);
+  const handlerClick = () => {
+    setClicked(true);
+    onClick();
+  };
+  return (
+    <div
+      className={clicked === false ? styles.cell : styles.clickedCell}
+      onClick={handlerClick}
+    ></div>
+  );
 }
 
 export default Cell;
--- src/PlayingField/index.tsx
+++ src/PlayingField/index.tsx
@@ -1,9 +1,12 @@
 import styles from "./index.module.scss";
+import { useState } from "react";
 import Cell from "../Cell";
+
 function PlayingField() {
+  const [clickedCell, setClickedCell] = useState<boolean>(false);
   const cells = [];
   for (let i = 0; i < 256; i++) {
-    cells.push(<Cell />);
+    cells.push(<Cell onClick={() => console.log(20)} />);
   }
 
   return <div className={styles.PlayingField}>{cells}</div>;
